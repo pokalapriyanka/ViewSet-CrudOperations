4f3c7e5629201d135dcc25eee6c670905315f61e
pokalapriyanka <pokalapriyanka123@gmail.com>
2024-07-09 10:31:22 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.viewsets import ViewSet
+from app.models import *
+from app.serializers import *
+from rest_framework.response import Response
+
+class ProductCrudView(ViewSet):
+    def list(self,request):
+        LPO=Product.objects.all()
+        MSPO=ProductMS(LPO,many=True)
+        return Response(MSPO.data)
+
+    def retrieve(self,request,pk):
+        PO=Product.objects.get(pid=pk)
+        JO=ProductMS(PO)
+        return Response(JO.data)
